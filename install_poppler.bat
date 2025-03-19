8e3062a1cf6dcab7f345de4b2853cd06549f46b8
Paul ARNAUD <arnaudpaul15@gmail.com>
2025-03-19 09:51:03 +0100

    suppression du venv du dépot distant + modif script installation bat

--- install_poppler.bat
+++ install_poppler.bat
@@ -1,43 +1,46 @@
 @echo off
-:: Définir le chemin temporaire et le dossier d'extraction
+:: Définir le dossier où se trouve le script
 set "TEMP_DIR=%~dp0"
 set "POPPLER_DIR=C:\poppler-24.07.0"
+set "ZIP_FILE=Release-24.07.0-0.zip"
 
 :: Vérifier si le fichier ZIP de Poppler est présent dans le dossier du script
-if not exist "%TEMP_DIR%\Release-24.07.0-0.zip" (
-    echo Fichier Poppler ZIP non trouvé dans le dossier du script.
+if not exist "%TEMP_DIR%\%ZIP_FILE%" (
+    echo Fichier ZIP de Poppler non trouvé dans le dossier du script.
     exit /b 1
 )
 
-:: Extraire Poppler
-echo Extraction de Poppler...
-powershell -Command "Expand-Archive -Path '%TEMP_DIR%\Release-24.07.0-0.zip' -DestinationPath 'C:\'"
-
-:: Vérifier si l'extraction a réussi
-if not exist "%POPPLER_DIR%" (
-    echo Échec de l'extraction de Poppler.
-    exit /b 1
+:: Vérifier si Poppler est déjà installé
+if exist "%POPPLER_DIR%" (
+    echo Poppler semble déjà être installé dans %POPPLER_DIR%.
+) else (
+    :: Extraire Poppler
+    echo Extraction de Poppler...
+    powershell -Command "Expand-Archive -Path '%TEMP_DIR%\%ZIP_FILE%' -DestinationPath 'C:\'"
+    
+    :: Vérifier si l'extraction a réussi
+    if not exist "%POPPLER_DIR%" (
+        echo Échec de l'extraction de Poppler.
+        exit /b 1
+    ) else (
+        echo Poppler extrait avec succès !
+    )
 )
 
-:: Ajouter Poppler au PATH si ce n'est pas déjà présent
-echo Ajout de Poppler au PATH...
+:: Définir le chemin de la bibliothèque Poppler
 set "NEW_PATH=%POPPLER_DIR%\Library\bin"
 
-:: Vérifier si le chemin de Poppler est déjà dans le PATH
-echo %PATH% | find /i "%NEW_PATH%" >nul
+:: Vérifier si Poppler est déjà dans le PATH de l'utilisateur
+for /f "tokens=2*" %%A in ('reg query "HKCU\Environment" /v PATH 2^>nul') do set "USER_PATH=%%B"
+
+echo %USER_PATH% | find /i "%NEW_PATH%" >nul
 if errorlevel 1 (
-    :: Poppler n'est pas dans le PATH, donc on l'ajoute
-    setx PATH "%PATH%;%NEW_PATH%"
-    echo Poppler ajouté au PATH.
+    echo Ajout de Poppler au PATH de l'utilisateur...
+    setx PATH "%USER_PATH%;%NEW_PATH%"
+    echo Poppler a été ajouté au PATH de l'utilisateur.
 ) else (
-    echo Poppler est déjà dans le PATH.
+    echo Poppler est déjà dans le PATH de l'utilisateur.
 )
 
-echo Poppler installé avec succès !
-
-:: Nettoyage des fichiers temporaires
-echo Nettoyage des fichiers temporaires...
-del "%TEMP_DIR%\Release-24.07.0-0.zip"
-
-echo Installation terminée.
+echo Installation terminée avec succès !
 pause
